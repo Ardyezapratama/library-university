ec71acf7c1d1484c35b7c7fb71d61028a715571b
Ardyezapratama <ardyezapratama@gmail.com>
2025-11-04 10:13:24 +0800

    Implement Auth UI

---
+++ components/AuthForm.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+	Controller,
+	DefaultValues,
+	FieldValues,
+	Path,
+	SubmitHandler,
+	useForm,
+	UseFormReturn,
+} from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+	Field,
+	FieldError,
+	FieldGroup,
+	FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
+import Link from "next/link";
+import { FIELD_NAMES, FIELD_TYPES } from "@/constants";
+import ImageUpload from "./ImageUpload";
+
+interface Props<T extends AuthFormData> {
+	schema: z.ZodType<T, any>;
+	defaultValues: T;
+	onSubmit: (data: T) => Promise<{ success: boolean; error?: string }>;
+	type: "SIGN_IN" | "SIGN_UP";
+}
+
+type AuthFormData = {
+	[key: string]: any;
+};
+
+const AuthForm = <T extends AuthFormData>({
+	type,
+	schema,
+	defaultValues,
+	onSubmit,
+}: Props<T>) => {
+	const isSignIn = type === "SIGN_IN";
+
+	const form: UseFormReturn<T> = useForm({
+		resolver: zodResolver(schema),
+		defaultValues: defaultValues as DefaultValues<T>,
+	});
+
+	const handleSubmit: SubmitHandler<T> = async (data) => {};
+	return (
+		<div className="flex flex-col gap-4">
+			<h1 className="text-2xl font-semibold text-white">
+				{isSignIn ? "Welcome back to BookWise" : "Create your library account"}
+			</h1>
+			<p className="text-light-100">
+				{isSignIn
+					? "Access the vast collection of resources, and stay updated"
+					: "Please complete all fields and upload a valid university ID to gain access to the libarary"}
+			</p>
+			<form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-6">
+				{Object.keys(defaultValues).map((field) => (
+					<FieldGroup key={field}>
+						<Controller
+							name={field as Path<T>}
+							control={form.control}
+							render={({ field, fieldState }) => (
+								<Field data-invalid={fieldState.invalid}>
+									<FieldLabel htmlFor="fullName" className="capitalize">
+										{FIELD_NAMES[field.name as keyof typeof FIELD_NAMES]}
+									</FieldLabel>
+									{field.name === "universityCard" ? (
+										<ImageUpload />
+									) : (
+										<Input
+											required
+											id="fullName"
+											aria-invalid={fieldState.invalid}
+											type={FIELD_TYPES[field.name as keyof typeof FIELD_TYPES]}
+											{...field}
+											className="form-input"
+										/>
+									)}
+									{fieldState.invalid && (
+										<FieldError errors={[fieldState.error]} />
+									)}
+								</Field>
+							)}
+						/>
+					</FieldGroup>
+				))}
+
+				<Button type="submit" className="form-btn">
+					{isSignIn ? "Sign In" : "Sign Up"}
+				</Button>
+			</form>
+
+			<p className="text-center text-base font-medium">
+				{isSignIn ? "New to BookWise? " : "Already have an account? "}
+				<Link
+					href={isSignIn ? "/sign-up" : "/sign-in"}
+					className="font-bold text-primary"
+				>
+					{isSignIn ? "Create an account" : "Sign in"}
+				</Link>
+			</p>
+		</div>
+	);
+};
+
+export default AuthForm;
